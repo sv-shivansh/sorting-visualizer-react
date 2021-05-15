2077da65f4a1279a8645b5b99b0cdec25f182c8f
sv-shivansh <sv_shivansh@outlook.com>
2021-05-15 16:47:41 +0530

    merge sort implemented

--- src/components/SortingVisualizer.js
+++ src/components/SortingVisualizer.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-loop-func */
 import React, { useState, useEffect, useCallback } from "react";
-import BubbleSort from '../sorting-algos/BubbleSort'
+import BubbleSort from "../sorting-algos/BubbleSort";
+import MergeSort from "../sorting-algos/MergeSort";
 import "./css/SortingVisualizer.css";
 
 const SortingVisualizer = () => {
@@ -9,7 +10,7 @@ const SortingVisualizer = () => {
   const resetArray = useCallback(() => {
     var i = 0;
     var x = [];
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < 120; i++) {
       x.push(getRandomArbitrary(10, 500));
     }
     setArr(x);
@@ -24,39 +25,63 @@ const SortingVisualizer = () => {
     resetArray();
   }, [resetArray]);
 
-  function bubbleSort() { 
+  function bubbleSort() {
     const animations = BubbleSort(arr);
     const prevAnimation = [];
-    for(let i=0; i<animations.length; i++ ){
-      prevAnimation.push(animations[i].comparision)
-      prevAnimation.push(animations[i].comparision)
-      prevAnimation.push(animations[i].swap)
+    for (let i = 0; i < animations.length; i++) {
+      prevAnimation.push(animations[i].comparision);
+      prevAnimation.push(animations[i].comparision);
+      prevAnimation.push(animations[i].swap);
     }
-      for(let i = 0; i< prevAnimation.length; i++){
-        var bars = document.getElementsByClassName('array-bars');
-        const colorChange = (i % 3 !== 2)
-        if(colorChange){
-          const color = (i % 3 === 0)? 'green': 'rgb(255, 0, 234)'
-          const [one, two] = prevAnimation[i]
-          setTimeout(() => {
-            bars[one].style.backgroundColor = color;
-            bars[two].style.backgroundColor = color;
-          }, i*DELAY)
-        }
-        else{
-          setTimeout(() => {
-            const [one, two] = prevAnimation[i]
-            const tempHeight = bars[two].style.height;
-            bars[two].style.height = `${bars[one].style.height}`;
-            bars[one].style.height = `${tempHeight}`
-          }, i*DELAY)
-        }
+    for (let i = 0; i < prevAnimation.length; i++) {
+      var bars = document.getElementsByClassName("array-bars");
+      const colorChange = i % 3 !== 2;
+      if (colorChange) {
+        const color = i % 3 === 0 ? "green" : "rgb(255, 0, 234)";
+        const [one, two] = prevAnimation[i];
+        setTimeout(() => {
+          bars[one].style.backgroundColor = color;
+          bars[two].style.backgroundColor = color;
+        }, i * DELAY);
+      } else {
+        setTimeout(() => {
+          const [one, two] = prevAnimation[i];
+          const tempHeight = bars[two].style.height;
+          bars[two].style.height = `${bars[one].style.height}`;
+          bars[one].style.height = `${tempHeight}`;
+        }, i * DELAY);
       }
+    }
   }
-  
-  function quickSort() {alert('Working on this functionality')}
 
-  function mergeSort() {alert('Working on this functionality')}
+  function quickSort() {
+    alert("Working on this functionality");
+  }
+
+  function mergeSort() {
+    const animations = MergeSort(arr);
+    console.log(animations)
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bars');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? 'green' : 'rgb(255, 0, 234)';
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * DELAY);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * DELAY);
+      }
+    }
+  }
 
   return (
     <div className="array-container">
@@ -67,7 +92,7 @@ const SortingVisualizer = () => {
           style={{ height: `${value}px` }}
         ></div>
       ))}
-      <div className='button-container'>
+      <div className="button-container">
         <button onClick={() => resetArray()}>Generate new Array</button>
         <button onClick={() => bubbleSort()}>Bubble Sort</button>
         <button onClick={() => mergeSort()}>Merge Sort</button>
